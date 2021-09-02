9a87b4f056e64f88ec84a109a6863f5846389f42
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-09-02 10:37:24 +0200

    v. 1.0

--- javascript/index.js
+++ javascript/index.js
@@ -2,56 +2,56 @@ var events = [
     {
         name: "Kegeln",
         isAvailable: function () {
-            if (!daytime || (weekdays && daytime)) return true;
+            if (!daytime || (weekday && daytime)) return true;
             return false;
         }
     },
     {
         name: "Freibad",
         isAvailable: function () {
-            if (goodWeather && daytime) return true;
+            if (weather && daytime) return true;
             return false;
         }
     },
     {
         name: "Hallenbad",
         isAvailable: function () {
-            if (holidays && !weekdays) return true;
+            if (holiday && !weekday) return true;
             return false;
         }
     },
     {
         name: "Wandern im Dunkelwald",
         isAvailable: function () {
-            if (goodWeather && daytime) return true;
+            if (weather && daytime) return true;
             return false;
         }
     },
     {
         name: "Freikurs Musik",
         isAvailable: function () {
-            if (!daytime && !holidays && !weekdays) return true;
+            if (!daytime && !holiday && !weekday) return true;
             return false;
         }
     },
     {
         name: "Brotbackkurs",
         isAvailable: function () {
-            if (!goodWeather && !weekdays) return true;
+            if (!weather && !weekday) return true;
             return false;
         }
     },
     {
         name: "Schieferbergwerk",
         isAvailable: function () {
-            if (!weekdays && !daytime && holidays) return true;
+            if (!weekday && !daytime && holiday) return true;
             return false;
         }
     },
     {
         name: "Go-Kurse",
         isAvailable: function () {
-            if ((!weekdays && !goodWeather) || (!daytime && goodWeather))
+            if ((!weekday && !weather) || (!daytime && weather))
                 return true;
             return false;
         }
@@ -59,28 +59,28 @@ var events = [
     {
         name: "Billard",
         isAvailable: function () {
-            if (daytime && !weekdays & !kids) return true;
+            if (daytime && !weekday & !kid) return true;
             return false;
         }
     },
     {
         name: "Rennautofahren",
         isAvailable: function () {
-            if (!kids && !weekdays && holidays) return true;
+            if (!kid && !weekday && holiday) return true;
             return false;
         }
     },
     {
         name: "Open-Air-Kino",
         isAvailable: function () {
-            if (goodWeather && (daytime && weekdays)) return true;
+            if (weather && (daytime && weekday)) return true;
             return false;
         }
     },
     {
         name: "Korbflechten",
         isAvailable: function () {
-            if (holidays && !goodWeather && weekdays) return true;
+            if (holiday && !weather && weekday) return true;
             return false;
         }
     },
@@ -103,44 +103,19 @@ var events = [
 });
 
 function check() {
-    weekdays = document.getElementById("weekdays").checked;
-    goodWeather = document.getElementById("goodWeather").checked;
-    holidays = document.getElementById("holidays").checked;
+    weekday = document.getElementById("weekday").checked;
+    weather = document.getElementById("weather").checked;
+    holiday = document.getElementById("holiday").checked;
     daytime = document.getElementById("daytime").checked;
-    kids = document.getElementById("kids").checked;
-    out = document.getElementById("out");
+    kid = document.getElementById("kid").checked;
+    output = document.getElementById("output");
 
-    console.log(weekdays, goodWeather, holidays, daytime, kids)
-
-    $.getJSON("../events.json", function(json) {
-        console.log(json); 
-
-        const data = json.items;
-
-        data.forEach(function(data, index){
-
-            if(weekdays == true, goodWeather == true, holidays == true, daytime == true, kids == true){
-                if (data.weather =="all"|| data.weather=="gut"||data.)
-            }
-
-            if(data.weather == "all"){
-                console.log(data);
-            }
-
-        });
-
-    });
-
-
-    
-
-
-
+    console.log(weekday, weather, holiday, daytime, kid)
     var arr = [];
     var i = 0;
     for (i; i < events.length; i++)
         if (events[i].isAvailable()) arr.push(events[i].name);
-    out.innerHTML = arr.sort().join(", ");
+    output.innerHTML = arr.sort().join(", ");
 
 
 }
